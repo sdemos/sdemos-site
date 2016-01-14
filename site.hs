1d897f7a8fe60b8ef5028136a88b00da78fac9aa
Stephen Demos <stphndemos@gmail.com>
2016-01-14 16:29:38 -0500

    Cleaned up site.hs haskell

--- site.hs
+++ site.hs
@@ -1,62 +1,75 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
 
+--------------
+-- Contexts --
+--------------
 
---------------------------------------------------------------------------------
-main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+postCtx :: Context String
+postCtx  = dateField "date" "%B %e, %Y"
+ `mappend` defaultContext
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+archiveCtx :: [Item String] -> Context String
+archiveCtx posts = listField "posts" postCtx (return posts)
+         `mappend` constField "title" "Archives"
+         `mappend` defaultContext
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+indexCtx :: Context String
+indexCtx = constField "title" "Stephen Demos"
+ `mappend` defaultContext
+
+------------
+-- Routes --
+------------
+
+images :: Rules ()
+images = match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
+css :: Rules ()
+css = match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
+
+posts :: Rules ()
+posts = match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+archive :: Rules ()
+archive = create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        makeItem ""
+            >>= loadAndApplyTemplate "templates/archive.html" (archiveCtx posts)
+            >>= loadAndApplyTemplate "templates/default.html" (archiveCtx posts)
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+index :: Rules ()
+index = match "index.html" $ do
+    route idRoute
+    compile $ getResourceBody
+            >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+templates :: Rules ()
+templates = match "templates/*" $ compile templateCompiler
 
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Stephen Demos"       `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
-
+----------
+-- Main --
+----------
+main :: IO ()
+main = hakyll $ do
+    images
+    css
+    posts
+    archive
+    index
+    templates
